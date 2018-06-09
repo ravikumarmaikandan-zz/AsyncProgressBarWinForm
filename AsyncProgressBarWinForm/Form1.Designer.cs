c80efa0e0b4d36a71e2debe4a68836d902df9c59
Ravikumar Maikandan <ravimike@live.com>
2018-06-09 17:33:03 -0500

    Initial commit

---
+++ AsyncProgressBarWinForm/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace AsyncProgressBarWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLaunch = new System.Windows.Forms.Button();
+            this.pgbStatus = new System.Windows.Forms.ProgressBar();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnLaunch
+            // 
+            this.btnLaunch.Location = new System.Drawing.Point(93, 39);
+            this.btnLaunch.Name = "btnLaunch";
+            this.btnLaunch.Size = new System.Drawing.Size(75, 23);
+            this.btnLaunch.TabIndex = 0;
+            this.btnLaunch.Text = "Start";
+            this.btnLaunch.UseVisualStyleBackColor = true;
+            this.btnLaunch.Click += new System.EventHandler(this.btnLaunch_Click);
+            // 
+            // pgbStatus
+            // 
+            this.pgbStatus.Location = new System.Drawing.Point(45, 98);
+            this.pgbStatus.Name = "pgbStatus";
+            this.pgbStatus.Size = new System.Drawing.Size(184, 16);
+            this.pgbStatus.TabIndex = 1;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(114, 79);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblStatus.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.pgbStatus);
+            this.Controls.Add(this.btnLaunch);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLaunch;
+        private System.Windows.Forms.ProgressBar pgbStatus;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ AsyncProgressBarWinForm/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncProgressBarWinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLaunch_Click(object sender, EventArgs e)
+        {
+            var progress = new Progress<int>(val =>
+            {
+                pgbStatus.Value = val;
+                lblStatus.Text = val.ToString() + "%";
+            });
+
+            LongRunningTask(progress);
+
+        }
+
+
+        private void LongRunningTask(IProgress<int> progress)
+        {
+            Task.Run(() =>
+            {
+                RunJob(progress);
+
+                return true;
+            });
+            return;
+        }
+
+        private void RunJob(IProgress<int> progress)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                Thread.Sleep(100);
+                if (progress != null)
+                    progress.Report(i);
+            }
+        }
+    }
+}
